ad6458b9f41aa45e0ac325863f248999d1fd1f7e
Sahil Amirov <69482519+SahilEmirov@users.noreply.github.com>
2023-06-03 14:14:23 +0300

    manager

--- Hackaton/Bussines/Concrete/DepartmentManager.cs
+++ Hackaton/Bussines/Concrete/DepartmentManager.cs
@@ -1,8 +1,17 @@
 ﻿using Hackaton.Bussines.Abstract;
+using Hackaton.DAL.Abstract;
 
 namespace Hackaton.Bussines.Concrete
 {
     public class DepartmentManager: IDepartmentService
     {
+        private readonly IDepartmentDal _departmentDal;
+
+        public DepartmentManager(IDepartmentDal departmentDal)
+        {
+            _departmentDal = departmentDal;
+        }
+
+
     }
 }
--- Hackaton/Bussines/Concrete/TheClassManager.cs
+++ Hackaton/Bussines/Concrete/TheClassManager.cs
@@ -1,8 +1,17 @@
 ﻿using Hackaton.Bussines.Abstract;
+using Hackaton.DAL.Abstract;
 
 namespace Hackaton.Bussines.Concrete
 {
     public class TheClassManager: ITheClassService
     {
+        private readonly ITheClassDal _theClassDal;
+
+        public TheClassManager(ITheClassDal theClassDal)
+        {
+            _theClassDal = theClassDal;
+        }
+
+
     }
 }
