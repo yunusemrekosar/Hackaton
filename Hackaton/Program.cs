4af63a6bee3d1cf05c3f2fdc40af02f7756706b0
Sahil Amirov <69482519+SahilEmirov@users.noreply.github.com>
2023-06-03 11:07:34 +0300

    biseyler

--- Hackaton/Program.cs
+++ Hackaton/Program.cs
@@ -1,4 +1,5 @@
 using Hackaton.Data;
+using Hackaton.Data.Entity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<UserApp>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<RoleApp>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
