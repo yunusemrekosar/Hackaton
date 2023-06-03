ca5fe105f29bf6ea83992d023511fb39a10266fd
Sahil Amirov <69482519+SahilEmirov@users.noreply.github.com>
2023-06-03 20:50:26 +0300

    asdasda

--- Hackaton/Models/UserApp/UpdateUserAppModel.cs
+++ Hackaton/Models/UserApp/UpdateUserAppModel.cs
@@ -10,7 +10,6 @@
         public string Country { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime Birthday { get; set; }
-        public string Password { get; set; }
         public int DepartmentId { get; set; }
     }
 }
