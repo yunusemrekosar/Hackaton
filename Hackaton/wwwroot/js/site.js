a672c60013e47470cad94b495850ca66ab670104
Ali Duru <aliduruu06@gmail.com>
2023-06-03 16:55:38 +0300

    init-2

--- Hackaton/wwwroot/js/site.js
+++ Hackaton/wwwroot/js/site.js
@@ -3,14 +3,15 @@
 
 // Write your JavaScript code.
 
+const main = document.querySelector('header');
+const reg = document.querySelector('.registerCon');
+const logging = document.querySelector('.loginCon');
+
 const registerOpen = () => {
-	let btn = document.querySelector('.register');
-	let main = document.querySelector('header');
-	let reg = document.querySelector('.registerCon');
-	btn.addEventListener('click', () => {
-		main.classList.toggle('blur');
-		reg.classList.toggle('hidden');
-		reg.innerHTML = `
+	main.classList.add('blur');
+	reg.classList.remove('hidden');
+	console.log('1');
+	reg.innerHTML = `
         <div
 				class="d-grid bg-transparent justify-content-center align-items-center vh-100 vw-100 z-5  gap-3 ">
 				<div class="col d-flex flex-column gap-5 " style='z-index:10'>
@@ -50,16 +51,58 @@ const registerOpen = () => {
 				</div>
 			</div>
         `;
-	});
 };
 
 const registerClose = () => {
-	let main = document.querySelector('header');
-	let reg = document.querySelector('.registerCon');
-	let btn = document.querySelector('.close');
-	btn.addEventListener('click', () => {
-		main.classList.toggle('blur');
-		reg.classList.toggle('hidden');
-		reg.innerHTML = '';
-	});
+	main.classList.remove('blur');
+	reg.classList.add('hidden');
+	console.log('2');
+	reg.innerHTML = '';
+};
+
+const loginOpen = () => {
+	main.classList.add('blur');
+	logging.classList.remove('hidden');
+	console.log('1');
+	logging.innerHTML = `
+        <div
+				class="d-grid bg-transparent justify-content-center align-items-center vh-100 vw-100 z-5  gap-3 ">
+				<div class="col d-flex flex-column gap-5 " style='z-index:10'>
+					<div
+						class="d-flex gap-1 align-items-center justify-content-between">
+						<h3 class="text-white">Username</h3>
+						<input
+							type="text"
+							id="username" />
+					</div>
+					
+					<div
+						class="d-flex gap-1 align-items-center justify-content-between">
+						<h3 class="text-white">E-MAİL</h3>
+						<input
+							type="password"
+							id="password" />
+					</div>
+					<div
+						class="d-flex gap-1 align-items-center justify-content-between">
+						<a
+							class="nav-link btn btn-dark h-75 text-white close"
+							onclick="loginClose()"
+							>GERİ</a
+						>
+						<input
+							class="btn btn-light"
+							type="button"
+							value="GİRİŞ YAP" />
+					</div>
+				</div>
+			</div>
+        `;
+};
+
+const loginClose = () => {
+	main.classList.remove('blur');
+	logging.classList.add('hidden');
+	console.log('2');
+	logging.innerHTML = '';
 };
