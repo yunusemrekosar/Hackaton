abfb53b829a16f6fe0bf9a05826fba15c2e105af
Sahil Amirov <69482519+SahilEmirov@users.noreply.github.com>
2023-06-04 10:36:49 +0300

    adminrole

--- Hackaton/Controllers/AdminController.cs
+++ Hackaton/Controllers/AdminController.cs
@@ -3,21 +3,24 @@ using Hackaton.Core;
 using Hackaton.DAL.Abstract;
 using Hackaton.Data.Entity;
 using Hackaton.Models.UserApp;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hackaton.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         readonly UserManager<UserApp> _userManager;
         readonly SignInManager<UserApp> _signInManager;
-        readonly IUserAppService _userService;
+        readonly IUserAppService _userAppService;
 
-        public AdminController(SignInManager<UserApp> signInManager, UserManager<UserApp> userManager = null)
+        public AdminController(SignInManager<UserApp> signInManager, UserManager<UserApp> userManager = null, IUserAppService userAppService = null)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _userAppService = userAppService;
         }
 
         public IActionResult Index()
@@ -53,32 +56,48 @@ namespace Hackaton.Controllers
         public IActionResult TutorList()
         {
            
-            return View(_userService.GetTutors());
+            return View(_userAppService.GetTutors());
         }
 
         public IActionResult StudentList()
         {
-            return View(_userService.GetStudents());
+            return View(_userAppService.GetStudents());
         }
 
         public IActionResult AuditionList()
         {
-            return View(_userService.GetAuditionList());
+            return View(_userAppService.GetAuditionList());
         }
 
         [HttpPost]
         public IActionResult ChangeUserStatus(int UserId, int statusId)
         {
-            return View(_userService.ChangeUserStatus(UserId, statusId));
+            return View(_userAppService.ChangeUserStatus(UserId, statusId));
         }
 
         public IActionResult AddEditor()
         {
             return View();
         }
-        public IActionResult AddEditor(UserApp editör)
+
+        [HttpPost]
+        public async Task<IActionResult> AddEditor(UserApp editor)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                editor.UserStatusId = 9;
+
+                var result = await _userManager.CreateAsync(editor, CodeGenerator.RandomPassword(10)); //todo burada patlayabilirsin
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(editor, "Editor");
+                }
+
+                return NoContent();
+
+            }
+            return Content("burada 404 sayfasına yolla");
         }
 
     }
